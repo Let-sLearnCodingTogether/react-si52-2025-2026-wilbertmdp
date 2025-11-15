6de64ec963df105ee23b0f473bbcd86ad849ed13
wilbert <wilbertanderson_2327240015@mhs.mdp.ac.id>
2025-11-15 18:38:29 +0700

    pertemuan 15 november (gabung dengan movies express)
    install axios, tambah util ApiClient, dan function Movies, tambah useEffect, tambah useCallback dari situs react

--- src/pages/movies/Movies.tsx
+++ src/pages/movies/Movies.tsx
@@ -1,9 +1,37 @@
+import { useCallback, useEffect, useState } from "react"
 import { NavLink } from "react-router"
+import ApiClient from "../../utils/ApiClient"
+
+interface Movie {
+    _id : string,
+    judul : string,
+    tahunRilis : string,
+    sutradara : string,
+    createdAT : string,
+    updateAt : string
+}
 
 function Movies(){
-    return <div>
-        <h2>Movie Page</h2>
-        <NavLink to="/add-movie" className= "btn btn-primary">add-movie</NavLink>
+    const [Movies, setMovies] = useState<Movie[]>([])
+
+    const fetchMovies = useCallback(async() => {
+        const response = await ApiClient.get("/movie")
+
+        if(response.status == 200){
+            setMovies(response.data.data)
+        }
+
+    }, [])
+
+    useEffect(() => {
+        fetchMovies()
+    }, [fetchMovies])
+
+    return <div className="container mx-auto">
+        <div className="d-flex justify-content-between mb-3">
+            <h2>Movie Page</h2>
+            <NavLink to="/add-movie" className= "btn btn-primary">add-movie</NavLink>
+        </div>
     </div>
 }
 
